4bfb8165351ce65a50d5a078684abf881b1a7e8c
Michael Timbrook <timbrook480@gmail.com>
2022-04-03 22:19:26 -0700

    sure

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 mod types;
 
-use std::io::Read;
 use hyper::{body::Buf, Body, Client, Method, Request};
+use std::io::Read;
 
 use crate::types::{get_power_body, read_insight_response};
 
 #[derive(Debug)]
 enum WemoError {}
+
+#[derive(Debug)]
+struct Insight {
+    state: bool,
+    on_since: f32,
+    on_for: f32,
+    today_on_for: f32,
+    instant_power: f32,
+}
 
 async fn query_power_draw() -> Result<(), WemoError> {
     let request_power = Request::builder()
@@ -22,8 +31,7 @@ async fn query_power_draw() -> Result<(), WemoError> {
 
     let client = Client::new();
     let res = client.request(request_power).await.unwrap();
-
-    dbg!(res.status());
+    let status = res.status();
 
     let body = hyper::body::aggregate(res).await.unwrap();
     let mut reader = body.reader();
@@ -31,8 +39,34 @@ async fn query_power_draw() -> Result<(), WemoError> {
     let mut buffer = String::new();
     // Can probably skip this step and just use the reader
     reader.read_to_string(&mut buffer).unwrap();
-    read_insight_response(&buffer);
 
+    if !status.is_success() {
+        panic!("{}", buffer);
+    }
+
+    let insights = read_insight_response(&buffer);
+
+    println!("{}", insights);
+    // One line parsing is the way to go right?
+    let insight = match insights
+        .split("|")
+        .map(|v| v.parse().unwrap())
+        .collect::<Vec<f32>>()
+        .as_slice()
+    {
+        &[power_state, on_since, on_for, today_on_for, _e, _f, _g, instant_power, _i, _j, _k] => {
+            Insight {
+                state: power_state > 0.0,
+                on_for,
+                on_since,
+                today_on_for,
+                instant_power,
+            }
+        }
+        _ => panic!("Oh no"),
+    };
+
+    dbg!(insight);
 
     Ok(())
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,28 +1,28 @@
-use yaserde::{de::from_str, ser::to_string};
+use yaserde::{de::from_str, ser::to_string_with_config};
 use yaserde_derive::{YaDeserialize, YaSerialize};
 
 #[derive(Default, Debug, YaSerialize, YaDeserialize)]
 #[yaserde(
     prefix = "s",
-    namespace = "s: http://schemas.xmlsoap.org/soap/envelope/",
+    namespace = "s: http://schemas.xmlsoap.org/soap/envelope/"
     namespace = "u: urn:Belkin:service:insight:1"
 )]
 struct Envelope {
-    #[yaserde(rename = "Body", prefix = "s")]
-    body: GetInsightParams,
+    #[yaserde(rename = "Body", prefix = "s", default_namespace = "s")]
+    body: BodyParams,
 }
 
 #[derive(Debug, YaSerialize, YaDeserialize)]
-enum GetInsightParams {
+enum BodyParams {
     #[yaserde(prefix = "u")]
     GetInsightParams(InsightParams),
     #[yaserde(prefix = "u")]
     GetInsightParamsResponse(InsightParams),
 }
 
-impl Default for GetInsightParams {
+impl Default for BodyParams {
     fn default() -> Self {
-        GetInsightParams::GetInsightParamsResponse(InsightParams { child: None })
+        BodyParams::GetInsightParamsResponse(InsightParams { child: None })
     }
 }
 
@@ -32,30 +32,69 @@ struct InsightParams {
     child: Option<String>,
 }
 
-pub fn get_power_body() -> String {
-    let body = Envelope {
-        body: GetInsightParams::GetInsightParams(InsightParams { child: None }),
-    };
-    return to_string(&body).unwrap();
+mod response {
+    use yaserde_derive::{YaDeserialize, YaSerialize};
+
+    #[derive(Default, Debug, YaDeserialize, YaSerialize)]
+    #[yaserde(
+        prefix = "s",
+        namespace = "s: http://schemas.xmlsoap.org/soap/envelope/",
+        namespace = "urn:Belkin:service:metainfo:1"
+    )]
+    pub struct Envelope {
+        #[yaserde(prefix = "s", rename = "Body")]
+        pub body: Body,
+    }
+
+    #[derive(Default, Debug, YaDeserialize, YaSerialize)]
+    pub struct Body {
+        #[yaserde(prefix = "u", rename = "GetInsightParamsResponse")]
+        pub response: GetInsightParamsResponse,
+    }
+
+    #[derive(Default, Debug, YaDeserialize, YaSerialize)]
+    pub struct GetInsightParamsResponse {
+        #[yaserde(rename = "InsightParams")]
+        pub insight: String,
+    }
+
 }
 
-pub fn read_insight_response(buffer: &str) {
-    println!("\u{001b}[36m{:?}\u{001b}[0m", buffer);
-    let parsed: Envelope = from_str(&buffer).unwrap();
-    println!("{:?}", parsed);
+pub fn get_power_body() -> String {
+    let yaserde_cfg = yaserde::ser::Config {
+        perform_indent: true,
+        ..Default::default()
+    };
+    let body = Envelope {
+        body: BodyParams::GetInsightParams(InsightParams { child: None }),
+    };
+    return to_string_with_config(&body, &yaserde_cfg).unwrap();
+}
+
+pub fn read_insight_response(buffer: &str) -> String {
+    let parsed: response::Envelope = from_str(&buffer).unwrap();
+    String::from(parsed.body.response.insight)
 }
 
 #[cfg(test)]
 mod tests {
     use std::fs;
-
-    use crate::types::get_power_body;
+    use crate::types::{get_power_body, read_insight_response};
 
     #[test]
     fn test_serialize() {
         assert_eq!(
             fs::read_to_string("tests/data/get_power.xml").unwrap(),
             get_power_body()
+        );
+    }
+    
+    #[test]
+    fn test_deserialize() {
+        let buffer = fs::read_to_string("tests/data/get_power_response.xml").unwrap();
+        assert_eq!(
+            read_insight_response(&buffer),
+            "8|1648943256|0|0|63636|99560|294|0|0|51821989.000000|8000"
         );
     }
 }
