e99553a8adbda9ee8ac717551906174640f61ce3
Michael Timbrook <timbrook480@gmail.com>
2022-04-08 18:10:03 -0700

    Find more configs

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -9,8 +9,11 @@ pub struct Configuration {
 pub fn load_config() -> Result<Configuration, &'static str> {
     Config::builder()
         .add_source(File::with_name("settings"))
+        .add_source(File::with_name("local/settings"))
         .build()
-        .unwrap()
-        .try_deserialize::<Configuration>()
-        .map_err(|_| "Failed to parse config")
+        .map_err(|_| "Could not locate config")
+        .and_then(|c| {
+            c.try_deserialize::<Configuration>()
+                .map_err(|_| "Failed to parse config")
+        })
 }
